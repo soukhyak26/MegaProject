d0ec7f47eac1ebab4bf7d0586846dfd0d2497d41
Rahul Savaliya <rahulsavaliya@gmail.com>
2015-09-08 22:25:00 +0530

    Add ConsumerBasketActivationStatus enum type

---
+++ development/subscription/libs/axon-commons/src/main/java/com/affaince/subscription/common/type/ConsumerBasketActivationStatus.java
@@ -0,0 +1,27 @@
+package com.affaince.subscription.common.type;
+
+/**
+ * Created by rbsavaliya on 23-08-2015.
+ */
+public enum ConsumerBasketActivationStatus {
+    CREATED (0), ACTIVATED (1), EXPIRED (2);
+
+    private int statusCode;
+
+    ConsumerBasketActivationStatus(int statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public static ConsumerBasketActivationStatus valueOf (int statusCode){
+        switch (statusCode) {
+            case 0: return CREATED;
+            case 1: return ACTIVATED;
+            case 2: return EXPIRED;
+            default: return CREATED;
+        }
+    }
+}
