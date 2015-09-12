99e43c8f4014b012dad0b324f66a652d7a796b97
Mandar Kulkarni <kulkarnm@gmail.com>
2015-09-12 21:49:07 +0530

    Following changes are getting checked in 1. Requirements document is completely revised as per the changed scene 2. Integration: camel configuration is modified for better performance. test cases are written for validating few configuration.

--- development/subscription/tools/integration/src/main/java/com/affaince/subscription/integration/configuation/Axon.java
+++ development/subscription/tools/integration/src/main/java/com/affaince/subscription/integration/configuation/Axon.java
@@ -5,6 +5,7 @@ import com.affaince.subscription.integration.command.event.GenericEventPublisher
 import com.affaince.subscription.integration.command.event.basketdispatch.BasketDispatchedStatusEvent;
 import com.affaince.subscription.integration.command.event.itemreceipt.SubscriptionableItemReceivedEvent;
 import com.affaince.subscription.integration.command.event.shoppingitemreceipt.ShoppingItemReceivedEvent;
+import com.affaince.subscription.integration.command.event.stockdemand.SubscriptionableItemStockDemandGeneratedEvent;
 import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.jackson.JacksonDataFormat;
@@ -36,29 +37,35 @@ public class Axon extends Default {
     public RouteBuilder routes() {
         return new RouteBuilder() {
             public void configure() {
-
-                from("file:D://abc?fileName=subscriptionableItemsFeed.in").
+                //INT_02: retreive subscriptioable items details from main application
+                from("${subscriptionableitems.feed.source}").
                         unmarshal().bindy(BindyType.Csv, SubscriptionableItemReceivedEvent.class).
-                        split().body().
-                        to("bean:publisher");
+                        split(body().tokenize("\n")).streaming().
+                        to("${subscriptionableitems.feed.destination}");
 
                 JacksonDataFormat df = new JacksonDataFormat(objectMapper(), BasketDispatchedStatusEvent.class);
-                from("file:D://abc?fileName=BasketDispatchedStatus.in").
+                from("${basket.dispatch.status.source}").
                         unmarshal().bindy(BindyType.Csv, BasketDispatchedStatusEvent.class).
-                        split().body().
-                        to("bean:publisher");
+                        split(body().tokenize("\n")).streaming().
+                        to("${basket.dispatch.status.destination}");
 
-                from("file:D://abc?fileName=ShoppingItemsForRegistration.in").
+                //INT_01: retreive shopping items for registration
+                from("${shoppingitems.registration.source}").
                         unmarshal().bindy(BindyType.Csv, ShoppingItemReceivedEvent.class).
-                        split().body().
-                        to("bean:publisher");
+                        split(body().tokenize("\n")).streaming().
+                        to("${shoppingitems.registration.destination}");
 
-                from("bean:NotificationEvent").marshal("json").to("restlet:/notification?restletMethod=POST");
+                //INT_03: generate stock demand based on available subscripttions
+                from("${stock.demand.source}").
+                        //unmarshal().bindy(BindyType.Csv, ShoppingItemReceivedEvent.class).
+                                marshal().bindy(BindyType.Csv,SubscriptionableItemStockDemandGeneratedEvent.class).
+                        to("${stock.demand.destination}");
+
+                //from("bean:NotificationEvent").marshal("json").to("restlet:/notification?restletMethod=POST");
 
             }
         };
     }
-
 
     @Bean
     CamelContextConfiguration contextConfiguration() {
---
+++ development/subscription/tools/integration/src/test/java/configuration/SubscriptionableItemReceiptTest.java
@@ -0,0 +1,74 @@
+package configuration;
+
+import com.affaince.subscription.integration.command.event.itemreceipt.SubscriptionableItemReceivedEvent;
+import org.apache.camel.CamelContext;
+import org.apache.camel.EndpointInject;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.model.dataformat.BindyType;
+import org.apache.camel.test.spring.*;
+import org.joda.time.LocalDate;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.BootstrapWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.apache.camel.spring.javaconfig.SingleRouteCamelConfiguration;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by mandark on 12-09-2015.
+ */
+@RunWith(CamelSpringJUnit4ClassRunner.class)
+@BootstrapWith(CamelTestContextBootstrapper.class)
+@ContextConfiguration( classes = {SubscriptionableItemReceiptTest.ContextConfig.class},
+        loader = CamelSpringDelegatingTestContextLoader.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@MockEndpoints()
+public class SubscriptionableItemReceiptTest {
+    @EndpointInject(uri = "mock:queue.csv")
+    protected MockEndpoint mock;
+
+    @Configuration
+    public static class ContextConfig extends SingleRouteCamelConfiguration  {
+        @Bean
+        @Override
+        public RouteBuilder route(){
+            return new RouteBuilder() {
+                @Override
+                public void configure() throws Exception {
+                    from("file://src/test/resources?noop=true&fileName=subscriptionableItemsFeed.in").
+                            unmarshal().bindy(BindyType.Csv, SubscriptionableItemReceivedEvent.class).
+                            split(body().tokenize("\n")).streaming()
+                            .to("mock:queue.csv");
+                }
+            };
+        }
+    }
+
+    @DirtiesContext
+    @Test
+    public void testPositive() throws Exception {
+        SubscriptionableItemReceivedEvent event = new SubscriptionableItemReceivedEvent();
+        event.setItemId("31");
+        event.setBatchId("19072015");
+        event.setCategoryId("1111");
+        event.setCategoryName("CATEGORY1");
+        event.setSubCategoryId("23");
+        event.setSubCategoryName("SUBCATEGORY23");
+        event.setProductId("TOOTHPSTCOLGT");
+        event.setCurrentPurchasePricePerUnit(47.00);
+        event.setCurrentMRP(74.50);
+        event.setCurrentStockInUnits(23456);
+        event.setCurrentPriceDate(LocalDate.now());
+        mock.expectedMessageCount(1);
+        mock.expectedBodiesReceived(event.toString());
+        mock.assertIsSatisfied();
+    }
+
+}
