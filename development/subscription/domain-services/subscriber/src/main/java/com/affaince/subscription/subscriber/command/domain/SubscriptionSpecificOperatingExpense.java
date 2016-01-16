5f98794b6ca952d00c4412e0acaf9b11df59cefb
anayonkars <anayonkar.shivalkar@gmail.com>
2016-01-16 14:40:34 +0530

    Fixing typo in file name

--- development/subscription/domain-services/subscriber/src/main/java/com/affaince/subscription/subscriber/command/domain/SubscriptionSpecificOperatingExpense.java
+++ development/subscription/domain-services/subscriber/src/main/java/com/affaince/subscription/subscriber/command/domain/SubscriptionSpecificOperatingExpense.java
@@ -11,6 +11,7 @@ import java.util.List;
 /**
  * Created by rbsavaliya on 16-01-2016.
  */
+
 public class SubscriptionSpecificOperatingExpense extends AbstractAnnotatedAggregateRoot<String> {
 
     @AggregateIdentifier
