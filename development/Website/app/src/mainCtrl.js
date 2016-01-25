b5f4ce5858246d6900bb4f8f58b81bb0eb4cf08a
ultrasonicsoft <uvbrainsolutions@outlook.com>
2016-01-25 10:34:36 +0530

    Added menu options

--- development/Website/app/src/mainCtrl.js
+++ development/Website/app/src/mainCtrl.js
@@ -5,11 +5,10 @@ mainApp.controller('MainCtrl', function (userService, $mdSidenav, $mdBottomSheet
     var self = this;
     self.isLoggedIn = false;
     self.user = { email: "Balram", password: "Balram" };
-
+    self.configurations = [{ name: "Application" }, { name: "Product" }, { name: "Subscriber" }, { name: "Benefits" }, { name: "BAM" }];
     self.login = login;
     self.logout = logout;
     self.toggleList = toggleList;
-
 
     function logout() {
         self.isLoggedIn = false;
