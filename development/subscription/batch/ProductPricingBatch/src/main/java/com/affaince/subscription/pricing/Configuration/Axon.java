6c8846767005f2436f5c989fb05e662a96ffc6a1
kulkarnm <kulkarnm@gmail.com>
2016-07-03 14:53:04 +0530

    Added starter for ProductPricingBatch. Removed duplicate maven-compiler-plugins

---
+++ development/subscription/batch/ProductPricingBatch/src/main/java/com/affaince/subscription/pricing/Configuration/Axon.java
@@ -0,0 +1,55 @@
+package com.affaince.subscription.pricing.Configuration;
+
+import com.affaince.subscription.common.publisher.GenericEventPublisher;
+import com.affaince.subscription.configuration.RabbitMQConfiguration;
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.spring.boot.CamelContextConfiguration;
+import org.apache.camel.util.toolbox.AggregationStrategies;
+import org.axonframework.eventhandling.EventTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by mandark on 19-07-2015.
+ */
+@Configuration
+public class Axon extends RabbitMQConfiguration {
+
+    @Autowired
+    private CamelContext camelContext;
+
+    @Bean
+    public GenericEventPublisher publisher(EventTemplate template) {
+        return new GenericEventPublisher(template);
+    }
+    @Bean
+    public RouteBuilder routes() {
+        return new RouteBuilder() {
+            public void configure() {
+                from("timer://foo?repeatCount=1").choice()
+                        .to("bean:operatingExpenseStrategyDeterminator?decideOperatingExpenseStrategy()")
+                        .when(simple("${body.operatingExpenseDistributionStrategyType}==${type:com.affaince.subscription.expensedistribution.vo.OperatingExpenseDistributionStrategyType.DEFAULT_STRATEGY}"))
+                        .to("bean:defaultOperatingExpenseDistributionDeterminator")
+                        .when(simple("${body.operatingExpenseDistributionStrategyType}==${type:com.affaince.subscription.expensedistribution.vo.OperatingExpenseDistributionStrategyType.EXTRAPOLATION_BASED_STRATEGY}"))
+                        .to("bean:extraPolationBasedOperatingExpenseDistributionDeterminator")
+                        .when(simple("${body.operatingExpenseDistributionStrategyType}==${type:com.affaince.subscription.expensedistribution.vo.OperatingExpenseDistributionStrategyType.FORECAST_BASED_STRATEGY}"))
+                        .to("bean:forecastBasedOperatingExpenseDistributionDeterminator")
+                       // .multicast(AggregationStrategies.bean(ProductWiseDeliveryStatsAggregation.class))
+                        .to ("bean:calculatePerUnitExpense")
+                        .to("bean:publisher");
+            }
+        };
+    }
+
+    @Bean
+    CamelContextConfiguration contextConfiguration() {
+        return new CamelContextConfiguration() {
+            @Override
+            public void beforeApplicationStart(CamelContext context) {
+                System.out.println("@@@@@@@@@@@Hey Camel Started@@@@@@@@@@@");
+            }
+        };
+    }
+}
