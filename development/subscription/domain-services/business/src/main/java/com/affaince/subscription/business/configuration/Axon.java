d44ae9a8d098ff01a0b92a9ce4481ce3070529b1
kulkarnm <kulkarnm@gmail.com>
2017-02-20 12:31:27 +0530

    Removed unnecessary event mappings.

--- development/subscription/domain-services/business/src/main/java/com/affaince/subscription/business/configuration/Axon.java
+++ development/subscription/domain-services/business/src/main/java/com/affaince/subscription/business/configuration/Axon.java
@@ -53,22 +53,6 @@ public class Axon extends Default {
             put("com.affaince.subscription.subscriber.command.event.DeliveryStatusAndDispatchDateUpdatedEvent", DeliveryStatusAndDispatchDateUpdatedEvent.class.getName());
             //for debiting from the purchaseCostAccount for the delivered items;
             put("com.affaince.subscription.product.command.event.DeliveredSubscriptionCountAddedToPriceBucket", DeliveredSubscriptionCountAddedToPriceBucket.class.getName());
-            //put("com.affaince.subscription.business.command.event.*","");
-            /*put("com.affaince.subscription.business.command.event.BenefitCreditedEvent", BenefitCreditedEvent.class.getName());
-            put("com.affaince.subscription.business.command.event.BenefitDebitedEvent", BenefitDebitedEvent.class.getName());
-            put("com.affaince.subscription.business.command.event.BookingAmountCreditedEvent", BookingAmountCreditedEvent.class.getName());
-            put("com.affaince.subscription.business.command.event.BookingAmountDebitedEvent", BookingAmountDebitedEvent.class.getName());
-            put("com.affaince.subscription.business.command.event.CommonExpenseCreditedEvent", CommonExpenseCreditedEvent.class.getName());
-            put("com.affaince.subscription.business.command.event.CommonExpenseDebitedEvent", CommonExpenseDebitedEvent.class.getName());
-            put("com.affaince.subscription.business.command.event.CreateProvisionEvent", ProvisionCreatedEvent.class.getName());
-            put("com.affaince.subscription.business.command.event.DeliveryStatusAndDispatchDateUpdatedEvent", DeliveryStatusAndDispatchDateUpdatedEvent.class.getName());
-            put("com.affaince.subscription.business.command.event.OperatingExpenseReceivedEvent", OperatingExpenseReceivedEvent.class.getName());
-            put("com.affaince.subscription.business.command.event.PaymentProcessedEvent", PaymentProcessedEvent.class.getName());
-            put("com.affaince.subscription.business.command.event.PurchaseCostCreditedEvent", PurchaseCostCreditedEvent.class.getName());
-            put("com.affaince.subscription.business.command.event.PurchaseCostDebitedEvent", PurchaseCostDebitedEvent.class.getName());
-            put("com.affaince.subscription.business.command.event.SubscriptionActivatedEvent", SubscriptionActivatedEvent.class.getName());
-            put("com.affaince.subscription.business.command.event.SubscriptionSpecificExpenseCreditedEvent", SubscriptionSpecificExpenseCreditedEvent.class.getName());
-            put("com.affaince.subscription.business.command.event.SubscriptionSpecificExpenseDebitedEvent", SubscriptionSpecificExpenseDebitedEvent.class.getName());*/
         }};
     }
 }
