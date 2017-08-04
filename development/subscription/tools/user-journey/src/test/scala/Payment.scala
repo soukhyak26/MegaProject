71b4a546a9a3d61625629518baacf773664a0753
rbsavaliya <rahulsavaliya@gmail.com>
2017-08-04 10:27:15 +0530

    Gatling script for payment

---
+++ development/subscription/tools/user-journey/src/test/scala/Payment.scala
@@ -0,0 +1,39 @@
+import com.affaince.subscription.testdata.generator.ProductTestDataGenerator
+import io.gatling.core.Predef._
+import io.gatling.core.session.el._
+import io.gatling.http.Predef._
+
+import scala.util.Random
+
+/**
+  * Created by rbsavaliya on 05-03-2016.
+  */
+class Payment extends BaseSimulator {
+
+  var scn = scenario("Create Payment Scheme").exec(PaymentScheme.createPaymentScheme)
+
+  setUp(scn.inject(atOnceUsers(1)).protocols(http))
+}
+
+object PaymentScheme {
+
+  val createPaymentSchemeUrl = "http://localhost:8086/paymentscheme/create"
+
+  val createPaymentScheme = exec(
+      http("Create Payment Scheme")
+        .post(createPaymentSchemeUrl)
+        .body(
+          StringBody(
+            """
+              |{
+              |	"paymentSchemeName":"100%AdvancePayment",
+              |	"paymentSchemeDescription":"100% advance payment and default destribution of other payment",
+              |	"paymentSchemeRule":"pay 100% of current subscription amount on subscription confirmation and pay residual due amount after 1/2remaining-N, 3/4remaining-N delivery in default proportion",
+              |	"startDate":"2017-07-15",
+              |	"endDate":"2015-07-15"
+              |}
+            """.stripMargin
+          )
+        ).asJSON
+    )
+}
