c6b6ff082de4ce00211f1e67e36bdc3a8337f68b
kulkarnm <kulkarnm@gmail.com>
2017-08-16 15:00:03 +0530

    Created infrastructure for Spring cloud data flow... in progress

---
+++ development/subscription/tools/spring-cloud-dataflow-myshell/clean.bat
@@ -0,0 +1,7 @@
+app unregister --name http-source --type source
+
+app unregister --name transform-processor --type processor
+
+app unregister --name spring-cloud-sink --type sink
+
+stream destroy --name affiance-stream
