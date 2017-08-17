314d4f67ad8236b8c4d377e5b6aaf881cdf2a43f
kulkarnm <kulkarnm@gmail.com>
2017-08-17 20:24:05 +0530

    Removed prediction components from subscription project

--- development/subscription/tools/spring-cloud-dataflow-myshell/clean.bat
+++
@@ -1,7 +0,0 @@
-app unregister --name http-source --type source
-
-app unregister --name transform-processor --type processor
-
-app unregister --name spring-cloud-sink --type sink
-
-stream destroy --name affiance-stream
--- development/subscription/tools/spring-cloud-dataflow-myshell/dataflow-pipeline.bat
+++
@@ -1,9 +0,0 @@
-app register --name http-source --type source --uri maven://com.affaince:spring-cloud-source:jar:0.1-SNAPSHOT
-
-app register --name transform-processor --type processor --uri maven://com.affaince:spring-cloud-processor:jar:0.1-SNAPSHOT
-
-app register --name spring-cloud-sink --type sink --uri maven://com.affaince:spring-cloud-sink:jar:0.1-SNAPSHOT
-
-stream create --definition "http-source|transform-processor|spring-cloud-sink" --name affiance-stream
-
-stream deploy --name affiance-stream
