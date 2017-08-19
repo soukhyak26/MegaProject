6f98b1cd0819ecdfc47f547589f91acbb74757a5
rbsavaliya <rahulsavaliya@gmail.com>
2017-08-19 13:33:16 +0530

    Added PaymentScheme vo to test data generator

---
+++ development/subscription/tools/user-journey/src/test/java/com/affaince/subscription/testdata/generator/PaymentScheme.java
@@ -0,0 +1,9 @@
+package com.affaince.subscription.testdata.generator;
+
+public class PaymentScheme {
+    private String paymentSchemeId;
+
+    public void setPaymentSchemeId(String paymentSchemeId) {
+        this.paymentSchemeId = paymentSchemeId;
+    }
+}
