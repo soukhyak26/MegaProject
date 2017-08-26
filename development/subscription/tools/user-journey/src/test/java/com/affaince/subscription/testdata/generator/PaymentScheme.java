e3493b08f99da5b7bf9983be78aeb3e9f1fed650
rbsavaliya <rahulsavaliya@gmail.com>
2017-08-26 19:01:04 +0530

    resolve serialization issue

--- development/subscription/tools/user-journey/src/test/java/com/affaince/subscription/testdata/generator/PaymentScheme.java
+++ development/subscription/tools/user-journey/src/test/java/com/affaince/subscription/testdata/generator/PaymentScheme.java
@@ -1,9 +1,21 @@
 package com.affaince.subscription.testdata.generator;
 
+import ch.qos.logback.core.property.ResourceExistsPropertyDefiner;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class PaymentScheme {
     private String paymentSchemeId;
 
     public void setPaymentSchemeId(String paymentSchemeId) {
         this.paymentSchemeId = paymentSchemeId;
+    }
+
+    public String getPaymentSchemeId() {
+        return paymentSchemeId;
     }
 }
