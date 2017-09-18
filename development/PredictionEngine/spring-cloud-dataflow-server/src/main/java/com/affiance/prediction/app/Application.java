0860dc202a2d937864c449157fa6b74ca4416ae8
kulkarnm <kulkarnm@gmail.com>
2017-09-18 19:37:00 +0530

    updated stream definition..not working

--- development/PredictionEngine/spring-cloud-dataflow-server/src/main/java/com/affiance/prediction/app/Application.java
+++ development/PredictionEngine/spring-cloud-dataflow-server/src/main/java/com/affiance/prediction/app/Application.java
@@ -3,12 +3,14 @@ package com.affiance.prediction.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.dataflow.server.EnableDataFlowServer;
+import org.springframework.context.annotation.PropertySource;
 
 /**
  * Created by mandar on 8/15/2017.
  */
 @EnableDataFlowServer
 @SpringBootApplication
+@PropertySource({"classpath:Application.properties"})
 public class Application {
 
     public static void main(String[] args) {
