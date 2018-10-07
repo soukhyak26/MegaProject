0e8144f5689e5db3d6a4a18d2cc9a2dc86209b89
kulkarnm <kulkarnm@gmail.com>
2018-10-07 22:29:28 +0530

    Spike on ARIMAX..Done for demand of a product and its two substitutes

--- development/spikes/ARIMAX/src/com/example/ARIMAX/ARIMAXForecaster2.java
+++ development/spikes/ARIMAX/src/com/example/ARIMAX/ARIMAXForecaster2.java
@@ -1,44 +1,38 @@
 package com.example.ARIMAX;
 
-import breeze.linalg.Options;
 import breeze.storage.Zero;
-import com.cloudera.sparkts.models.ARIMA;
 import com.cloudera.sparkts.models.ARIMAX;
 import com.cloudera.sparkts.models.ARIMAXModel;
 import org.apache.commons.lang.ArrayUtils;
-import org.apache.spark.ml.linalg.Vectors;
 import org.apache.spark.mllib.linalg.DenseMatrix;
 import org.apache.spark.mllib.linalg.DenseVector;
-import scala.None$;
 import scala.Option;
 import scala.reflect.ClassTag;
 
 import java.io.*;
 import java.util.Arrays;
 
-import static scala.None$.MODULE$;
-
-public class ARIMAXForecaster {
+public class ARIMAXForecaster2 {
     public static void main(String[] args) throws FileNotFoundException, IOException {
-        double[] gdpTrain = getTrainData(3, 4);
-        double[] salesTrain = getTrainData(1, 2);
-        double[] adBudgetTrain = getTrainData(2, 3);
+        double[] M_demand = getTrainData(0, 1);
+        double[] S1_demand = getTrainData(1, 2);
+        double[] S2_demand = getTrainData(2, 3);
 
-        double[] gdpTest = getTestData(3, 4);
-        double[] salesTest = getTestData(1, 2);
-        double[] adBudgetTest = getTestData(2, 3);
+        double[] M_demandTest = getTestData(0, 1);
+        double[] S1_demandTest = getTestData(1, 2);
+        double[] S2_demandTest = getTestData(2, 3);
 
 
         ClassTag<Object> tag = scala.reflect.ClassTag$.MODULE$.apply(double.class);
         Zero zero = Zero.DoubleZero$.MODULE$;
-        DenseVector tsTrain = new DenseVector(gdpTrain);
-        DenseMatrix xregTrainSpark = new DenseMatrix(salesTrain.length,2,ArrayUtils.addAll(salesTrain,adBudgetTrain));
+        DenseVector tsTrain = new DenseVector(M_demand);
+        DenseMatrix xregTrainSpark = new DenseMatrix(S1_demand.length,2,ArrayUtils.addAll(S1_demand,S2_demand));
         breeze.linalg.DenseMatrix xregTrain = xregTrainSpark.asBreeze().toDenseMatrix(tag,zero);
-        DenseVector tsTestSpark = new DenseVector(gdpTest);
+        DenseVector tsTestSpark = new DenseVector(M_demandTest);
         breeze.linalg.DenseVector tsTest= tsTestSpark.asBreeze().toDenseVector(tag);
-        DenseMatrix xregTestSpark = new DenseMatrix(salesTest.length,2, ArrayUtils.addAll(salesTest,adBudgetTest));
+        DenseMatrix xregTestSpark = new DenseMatrix(S1_demandTest.length,2, ArrayUtils.addAll(S1_demandTest,S2_demandTest));
         breeze.linalg.DenseMatrix xregTest = xregTestSpark.asBreeze().toDenseMatrix(tag,zero);
-        ARIMAXModel arimaxModel = ARIMAX.fitModel(0, 0, 1, tsTrain, xregTrain, 1, true,true, scala.Option.apply(null));
+        ARIMAXModel arimaxModel = ARIMAX.fitModel(1, 0, 1, tsTrain, xregTrain, 1, true,true, Option.apply(null));
 
         breeze.linalg.DenseVector forecast = arimaxModel.forecast(tsTest, xregTest);
         for(int i=0; i< forecast.length();i++) {
@@ -50,24 +44,24 @@ public class ARIMAXForecaster {
     public static double[] getTrainData(int col1, int col2) {
         BufferedReader fileReader = null;
         try {
-            fileReader = new BufferedReader(new InputStreamReader(new FileInputStream(new File("E:\\apps\\affaince\\development\\spikes\\BoxJenkinsSpike\\data\\data_train.csv"))));
+            fileReader = new BufferedReader(new InputStreamReader(new FileInputStream(new File("E:\\apps\\affaince\\development\\spikes\\BoxJenkinsSpike\\data\\demands3.csv"))));
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
         String[] train = fileReader.lines().map(String::trim).toArray(String[]::new);
-        Double[] filteredArray = Arrays.stream(train).skip(1).flatMap(line -> Arrays.stream(Arrays.copyOfRange(line.split(",", 4), col1, col2))).map(s->Double.parseDouble(s)).toArray(Double[]::new);
+        Double[] filteredArray = Arrays.stream(train).skip(1).flatMap(line -> Arrays.stream(Arrays.copyOfRange(line.split(",", 3), col1, col2))).map(s->Double.parseDouble(s)).toArray(Double[]::new);
         return ArrayUtils.toPrimitive(filteredArray);
     }
 
     public static double[] getTestData(int col1, int col2) {
         BufferedReader fileReader = null;
         try {
-            fileReader = new BufferedReader(new InputStreamReader(new FileInputStream(new File("E:\\apps\\affaince\\development\\spikes\\BoxJenkinsSpike\\data\\data_test.csv"))));
+            fileReader = new BufferedReader(new InputStreamReader(new FileInputStream(new File("E:\\apps\\affaince\\development\\spikes\\BoxJenkinsSpike\\data\\demands4.csv"))));
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
         String[] train = fileReader.lines().map(String::trim).toArray(String[]::new);
-        Double[] filteredArray = Arrays.stream(train).skip(1).flatMap(line -> Arrays.stream(Arrays.copyOfRange(line.split(",", 4), col1, col2))).map(s->Double.parseDouble(s)).toArray(Double[]::new);
+        Double[] filteredArray = Arrays.stream(train).skip(1).flatMap(line -> Arrays.stream(Arrays.copyOfRange(line.split(",", 3), col1, col2))).map(s->Double.parseDouble(s)).toArray(Double[]::new);
         return ArrayUtils.toPrimitive(filteredArray);
     }
 }
