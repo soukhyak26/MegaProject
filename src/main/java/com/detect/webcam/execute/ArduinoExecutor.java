2d85e1dc5024dc84328be94fef76a0327b5edba1
soukhya <savya060313>
2023-04-25 22:42:55 +0530

    Arduino Executor is made to work by posting following code to the Arduino kit through IDE // Receives 65 from JAVA Programrunning on PC and lights up LED connected to Pin 7
    void setup()
    {
      pinMode(7, OUTPUT);            // sets the digital pin 7 as output
      Serial.begin(9600,SERIAL_8N1); // set up the arduino serial
    }
    
    void loop()
    {
    
        byte ReceivedByte = 0;
    
        if(Serial.available()>0)
        {
          ReceivedByte = Serial.read();
    
          if(ReceivedByte == 65)
          {
              digitalWrite(7, HIGH); // sets the digital pin 7 on
              delay(5000);            // waits for a second
              digitalWrite(7, LOW);  // sets the digital pin 7 off
              delay(1000);           // waits for a second
          }
    
        }//end of serial
    }//end of loop()

--- src/main/java/com/detect/webcam/execute/ArduinoExecutor.java
+++ src/main/java/com/detect/webcam/execute/ArduinoExecutor.java
@@ -1,17 +1,25 @@
 package com.detect.webcam.execute;
 
+import com.detect.webcam.capture.VideoRecorder;
 import com.fazecast.jSerialComm.SerialPort;
 
-import java.io.IOException;
 
 public class ArduinoExecutor {
-    public static void main(String[] args) throws IOException, InterruptedException {
+    private static SerialPort MySerialPort;
+
+    /*public static void main(String[] args) throws IOException, InterruptedException {
+        ArduinoExecutor arduinoExecutor = new ArduinoExecutor();
+        arduinoExecutor.setUp();
+        arduinoExecutor.communicateWithArduino();
+    }*/
+
+    public void setUp() throws InterruptedException{
         int BaudRate = 9600;
         int DataBits = 8;
         int StopBits = SerialPort.ONE_STOP_BIT;
         int Parity = SerialPort.NO_PARITY;
 
-        SerialPort MySerialPort = SerialPort.getCommPort("COM5"); //AvailablePorts[0];
+        this.MySerialPort = SerialPort.getCommPort("COM6"); //AvailablePorts[0];
 
         MySerialPort.setComPortParameters(BaudRate, DataBits, StopBits, Parity);//Sets all serial port parameters at one time
         MySerialPort.openPort(); //open the port
@@ -25,18 +33,22 @@ public class ArduinoExecutor {
         Thread.sleep(2000); //Delay introduced because when the SerialPort is opened ,Arduino gets resetted
         // Time for the code in Arduino to rerun after Reset
 
+    }
+    public static void communicateWithArduino(){
         try {
             byte[] WriteByte = new byte[1];
-            WriteByte[0] = 65; //send A
+            if(VideoRecorder.humanCount>0) {
+                WriteByte[0] = 65; //send A
+            }else{
+                WriteByte[0] = 20;
+            }
             int bytesTxed = 0;
             bytesTxed = MySerialPort.writeBytes(WriteByte, 1);
             System.out.print(" Bytes Transmitted -> " + bytesTxed);
-
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-        MySerialPort.closePort(); //Close the port
+        //MySerialPort.closePort(); //Close the port
 
         if (MySerialPort.isOpen())
             System.out.println(MySerialPort.getSystemPortName() + " is Open ");
